4e2f1798d6340d7059d7cd488d7278ce21b54637
Nikhil Limbu <nikhillimbu918@gmail.com>
2026-02-10 22:28:36 +0545

    feat: add not found route

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,15 @@ import "./index.css";
 import Layout from "./components/layout/Layout";
 import Home from "./components/pages/Home";
 import DetailedJob from "./components/JobSection/DetailedJob";
+import ComingSoon from "./components/ComingSoonSection/ComingSoon";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
       <Route path="/" element={<Home />} />
       <Route path="/job/:id" element={<DetailedJob />} />
+
+      <Route path="*" element={<ComingSoon />} />
     </Route>,
   ),
 );
